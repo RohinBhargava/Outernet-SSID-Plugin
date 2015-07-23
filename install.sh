85132fbdff47fd1b3fa5af8930a1d3f87de4a864
Rohin Bhargava <rohin@uchicago.edu>
2015-07-23 17:11:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 cp -r .ssid /mnt/data
 cp -r ssid /usr/lib/python2.7/site-packages/librarian/plugins
 cp S45hostapd /etc/init.d
+cp /etc/librarian.ini librarian.ini.bak
 cp librarian.ini /etc
 /etc/init.d/S90librarian restart
