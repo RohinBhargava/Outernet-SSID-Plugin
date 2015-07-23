634a4b8e408742b944e78bf89c489dfdc300f8fe
Rohin Bhargava <rohin@uchicago.edu>
2015-07-23 17:10:36 -0500

    Update install.pi.sh

--- install.pi.sh
+++ install.pi.sh
@@ -1,4 +1,5 @@
 cp -r .ssid /mnt/data
 cp -r ssid /usr/lib/python2.7/site-packages/librarian/plugins
+cp /usr/lib/python2.7/site-packages/librarian/librarian.ini librarian.ini.bak
 cp librarian.ini.pi /usr/lib/python2.7/site-packages/librarian/librarian.ini
 /etc/init.d/S91librarian restart
