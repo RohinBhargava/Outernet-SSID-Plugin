f79f4c2d021abe3737642f89e0bb474b5f1bf444
Rohin Bhargava <rohin@uchicago.edu>
2015-07-24 16:55:00 -0500

    Update plugin.py

--- ssid/plugin.py
+++ ssid/plugin.py
@@ -16,25 +16,27 @@ from bottle_utils.i18n import lazy_gettext as _, i18n_url
 from ..dashboard import DashboardPlugin
 from ...utils.template import view
 
-import os
+import subprocess
 
 @view('ssid/changed_results', error=None, name=None)
 def exec_command():
     name = request.query.name
+    reset = request.query.reset
     device = open('/etc/platform').read()
-    reciever_name = "Outernet"
+    receiver_name = "Outernet"
     path = None
     number = 0
 
     if name != '':
-        reciever_name += '-' + name
+        if reset == 'no':
+            receiver_name += '-' + name
 
         if 'ORxPi' in device:
             number = 81
             path = '/opt/orx'
             try:
-                os.sys('sed -i \'3s/ssid=.*/ssid=%s/g\' %s/hostapd.conf'%(receiver_name, path))
-            except OSError as err:
+                subprocess.call('sed -i \'3s/ssid=.*/ssid=%s/g\' %s/hostapd.conf'%(receiver_name, path), shell=True)
+            except subprocess.CalledProcessError as err:
                 return dict(error=err.message)
 
         elif 'Lighthouse' in device:
@@ -43,11 +45,10 @@ def exec_command():
 
         if path != None and number != 0:
             try:
-                os.sys('echo %s > %s/.ssid'%(reciever_name, path))
-                os.sys('/etc/init.d/S%dhostapd restart', nubmer)
-            except OSError as err:
+                subprocess.call('echo %s > %s/.ssid; /etc/init.d/S%dhostapd restart'%(receiver_name, path, number), shell=True)
+            except subprocess.CalledProcessError as err:
                 return dict(error=err.message)
-            return dict(name=reciever_name)
+            return dict(name=receiver_name)
 
         else:
             return dict(error='Something went wrong. Please try again. If the problem persists, please check your device name or contact your distributor.')
