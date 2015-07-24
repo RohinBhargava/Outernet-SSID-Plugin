d5d487fa664718568fb7c364413a3b0d244dee1e
Rohin Bhargava <rohin@uchicago.edu>
2015-07-24 16:52:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-cp -r .ssid /mnt/data
+echo Outernet > /mnt/persist/.ssid
 cp -r ssid /usr/lib/python2.7/site-packages/librarian/plugins
 cp /etc/init.d/S45hostapd S45hostapd.bak
 cp S45hostapd /etc/init.d
 cp /etc/librarian.ini librarian.ini.bak
-cp librarian.ini /etc
+vi librarian.ini
 /etc/init.d/S90librarian restart
